b5c05a9d9fbb4c5dad617cd63e395af0d53d2e7c
Lucas Napoli <lucas.napoli@gmail.com>
2020-01-18 23:08:30 -0500

    Testing Prototype Finish CensoredPrintClass Crated Prototype methods implemented in CensoredPrintWrite Printf (String, Arg) Println (String)
    Pending Char and Car[]

--- src/com/censored/lucas/TestCensor.java
+++ src/com/censored/lucas/TestCensor.java
@@ -1,5 +1,6 @@
 package com.censored.lucas;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Calendar;
@@ -17,42 +18,10 @@ public class TestCensor {
             cpw.setCensorTerms(censorTerms);
             writeData(cpw);
 
-
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
     }
-
-    static void writeData(CensoredPrintWriter pw) {
-        pw.println("-- Year end company report --");
-
-        Calendar c1 = Calendar.getInstance();
-        pw.format("Generated on: %tc / ", c1);
-        pw.format(Locale.CHINA, "%tc", c1);
-        pw.println();
-
-        pw.printf("Transfer from account %d to %d in the amount $1,000,000 #%d", 1356, 1268);
-        pw.println();
-
-        Calendar c2 = Calendar.getInstance();
-        c2.set(2019, 3, 12, 10, 13, 35);
-        pw.printf("Transfer took place on %tc / ", c2);
-        pw.printf(Locale.CHINA, "%tc", c2);
-        pw.println();
-
-        pw.println("Director Mark Branson announced his resignation");
-
-        pw.append("Our client ");
-        pw.append('G');
-        pw.write('o');
-        pw.print('o');
-        pw.print(new char[] {'g', 'l', 'e'});
-        pw.append(" had a positive response");
-
-        pw.close();
-
-    }
-
 
     static void writeData(PrintWriter pw) {
         pw.println("-- Year end company report --");
@@ -85,3 +54,4 @@ public class TestCensor {
 
 
 }
+
